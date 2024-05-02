31392c6ba5c6d7cbc7cb770ecc32c08aeb486e63
soyxbshxikh <you@example.com>
2024-05-02 15:37:32 +0530

    ne

--- Loop's/Assignment6.java
+++ Loop's/Assignment6.java
@@ -1,16 +1,15 @@
 class Assignment6 {
   public static void main(String[] args) {
 
-    int n = 10, firstTerm = 0, secondTerm = 1;
+    int n = 100, first = 0, second = 1;
     System.out.println("Fibonacci Series till " + n + " terms:");
 
     for (int i = 1; i <= n; ++i) {
-      System.out.print(firstTerm + ", ");
+      System.out.print(first + ", ");
 
-      // compute the next term
-      int nextTerm = firstTerm + secondTerm;
-      firstTerm = secondTerm;
-      secondTerm = nextTerm;
+      int next = first + second;
+      first = second;
+      second = next;
     }
   }
 }
