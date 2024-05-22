707e86a9c8b3499d3c3c8dd8f83eb8b0a882d973
soyxbshxikh <you@example.com>
2024-05-22 15:00:06 +0530

    new

---
+++ MultiDymentionArray/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a[][] = new int[2][2];
+        System.out.print("Enter four Numbers\n");
+        for(int i=0;i<2;i++)
+        {
+            for(int j=0;j<2;j++)
+            {
+                a[i][j]=sc.nextInt();
+            }
+        }
+        System.out.print("Your Matrix are\n");
+        for(int i=0;i<2;i++)
+        {
+            for(int j=0;j<2;j++)
+            {
+                System.out.print(a[j][i]+"\t");
+            }
+            System.out.println("\n");
+        }
+    }
+}
