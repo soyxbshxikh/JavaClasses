1f38e3150c0989a63f2e99f4c2a4630bac6d8398
soyab shaikh <soyxbshxaikh@gmail.com>
2024-05-24 17:10:11 +0530

    update

--- OOP Concepts/Keywords/Super3.java
+++ OOP Concepts/Keywords/Super3.java
@@ -1,10 +1,17 @@
-
-
-
-
-
-
-
-public class Super3 {
-    
+class Main
+{
+    Main() 
+        {
+            System.out.println("I am inn class A");
+        }
+}
+public class Super3 extends Main {
+    Super3()
+    {
+        super();
+        System.out.println("I am inn class B");
+    }
+    public static void main(String[] args) {
+        Super3 B = new Super3();
+    }
 }
