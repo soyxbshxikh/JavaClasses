538205f0d0478399be36a420cf531bef16e8c5ae
soyxbshxikh <you@example.com>
2024-06-22 15:32:43 +0530

    yup

--- Collection/ArrayList2.java
+++ Collection/ArrayList2.java
@@ -39,28 +39,28 @@ public class ArrayList2 {
                     System.out.print("Enter Product Name: ");
                     String addProduct = scanner.nextLine();
                     list.add(addProduct);
-                    System.out.println("Added Products: " + list);
+                    System.out.println("Products: " + list);
                     sc.productAdd(addProduct);
                     break;
                 case 2:
                     System.out.print("Remove Product: ");
                     String removeProduct = scanner.nextLine();
                     list.remove(removeProduct);
-                    System.out.println("After Remove: "+ list);
+                    System.out.println("Products: "+ list);
                     sc.productRemove(removeProduct);
                     break;
                 case 3:
                     System.out.print("Set Product: ");
                     String setProduct = scanner.nextLine();
                     list.set(0, setProduct);
-                    System.out.println("After Set: "+ list);
+                    System.out.println("Pruduct: "+ list);
                     sc.productSet(setProduct);
                     break;
                 case 4:
                     System.out.println("Thank you!");
                     break;
                 default:
-                    System.out.println("Invalid choice. Please choose 1, 2, 3, or 4.");
+                    System.out.println("Invalid choice, Please choose 1, 2, 3, 4.");
             }
         } while (choice != 4);
     }
